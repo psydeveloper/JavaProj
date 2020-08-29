643f3b6e22832784f4dd9ed7020c0d9fb4f612f9
beast <beast@beast>
2020-08-29 23:12:54 +0530

    floop

---
+++ Practice/src/practice/Floop.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Floop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//for(int i=1;i<5;i++) {
+	//		System.out.println(i);
+	//	}     OR
+	int n=5;
+		for(int i=0;i<5;i++) {
+			
+			for(int j=0;j<=i;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	
+	}
+
+}
--- Practice/src/practice/Wloop.java
+++ Practice/src/practice/Wloop.java
@@ -10,7 +10,8 @@ public class Wloop {
 			x++;	 
 			 
 			}
-		}}
+		}
+	}
 	
 
 
